fde9d883516fcb6878f84da8c977a58af6f60a26
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-05-03 19:53:40 -0700

    set up connections in master file

--- src/01_master.R
+++ src/01_master.R
@@ -7,3 +7,32 @@
 
 #***********************************************************
 
+library(odbc)
+library(dbplyr)
+library(magrittr)  
+library(tidyverse)
+
+# 1) parameters: --------------
+startdate_id <- '20180501'
+enddate_id <- '20180510'
+
+
+# 2) set up database connections: -----------
+cnx <- dbConnect(odbc::odbc(),
+                 dsn = "cnx_denodo_spappcsta001")
+
+
+vw_eddata <- dplyr::tbl(cnx, 
+                        dbplyr::in_schema("publish", 
+                                          "emergency"))
+vw_adtc <- dplyr::tbl(cnx, 
+                      dbplyr::in_schema("publish", 
+                                        "admission_discharge"))
+vw_census <- dplyr::tbl(cnx, 
+                        dbplyr::in_schema("publish", 
+                                          "census"))
+
+
+
+
+
