a95111953f4a580367526a4735ca98d7389f4fd7
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-05-06 14:28:31 -0700

    add nursing unit filter to census _denodo function

--- src/01_master.R
+++ src/01_master.R
@@ -14,7 +14,23 @@ library(tidyverse)
 
 # 1) parameters: --------------
 startdate_id <- '20180501'
-enddate_id <- '20180510'
+enddate_id <- '20180501'
+
+n_units_param <- c("LGH 2E",
+                   "LGH 3E", 
+                   "LGH 3W", 
+                   "LGH 4E",
+                   "LGH 4W",
+                   "LGH 5E",
+                   "LGH 6E",
+                   "LGH 6W",
+                   "LGH 7E",
+                   "LGH 7W", 
+                   "LGH 7W", 
+                   "LGH EIP", 
+                   "LGH ICU",
+                   "LGH MIU"
+                   )
 
 
 # 2) set up database connections: -----------
--- src/census-denodo_function.R
+++ src/census-denodo_function.R
@@ -8,30 +8,31 @@
 extract_census <- function(startdate_id, 
                            enddate_id, 
                            site = "Lions Gate Hospital", 
+                           n_units = n_units_param, 
                            denodo_vw = vw_census){
-      
-      
-      
       
       # inputs: 
       # > startdate and enddate as integers
       # > denodo_view is the name of the table saved as a connection via dbplyr 
       
       # outputs: 
-      # dataframe with census dates and census counts
+      # dataframe with census dates and census counts, by nursing unit
       
       vw_census %>% 
             filter(facility_name == !!(site),  # !! used to unquote the actual argument "site" 
                    census_date_id >= startdate_id, 
-                   census_date_id <= enddate_id) %>% 
+                   census_date_id <= enddate_id, 
+                   nursing_unit_cd %in% n_units) %>% 
             
             select(census_date_id, 
                    patient_id, 
+                   nursing_unit_cd, 
                    facility_name) %>%  # show_query()
             
             collect() %>% 
              
-            group_by(census_date_id) %>%
+            group_by(census_date_id, 
+                     nursing_unit_cd) %>%
             summarise(census = n())
       
       
@@ -41,5 +42,7 @@ extract_census <- function(startdate_id,
 
 
 # test the function: ------
+library(beepr)
+
 extract_census("20181212", 
-               "20181212")
+               "20181213"); beep()
