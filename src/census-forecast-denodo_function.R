11501215362d265d86fcbb836a9a6492dbaf85ed
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-05-08 10:48:49 -0700

    join past data on full date list

--- src/census-forecast-denodo_function.R
+++ src/census-forecast-denodo_function.R
@@ -5,13 +5,15 @@
 #*****************************************************
 
 # library(lubridate)
+# library(stringr)
+# library(tidyverse)
 
 # function definition: 
 census_forecast <- function(startdate_id, 
                             enddate_id, 
-                            past_years = 3, 
+                            past_years = 1,  # todo: change to 5?  
                             site = "Lions Gate Hospital", 
-                            n_unit = n_unit_param, 
+                            n_unit, 
                             denodo_vw = vw_census){
       
       # inputs: 
@@ -23,18 +25,19 @@ census_forecast <- function(startdate_id,
       # > dataframe with columns: date_id, nursing_unit_cd, metric, value
       # > levels of "metric" field: predicted, prediction_lower, prediction_upper
       
-      library(lubridate)
-      library(stringr)
-      
 
       # check whether function to pull census from denodo has been loaded: 
       stopifnot(exists("extract_census"))
       
       
       # historical data range: 
-      historical_start <- ymd(startdate_id) -1 - years(past_years) %>% print
-      historical_end <- ymd(startdate_id) -1 %>% print
+      historical_start <- lubridate::ymd(startdate_id) -1 - years(past_years)
+      historical_end <- lubridate::ymd(startdate_id) -1 
       
+      print(c(historical_start, 
+              historical_end))
+      
+      # start and end as date_ids: 
       historical_start_id <- 
             historical_start %>% 
             as.character( ) %>% 
@@ -50,23 +53,26 @@ census_forecast <- function(startdate_id,
             data.frame(date = seq(historical_start, 
                                   historical_end, 
                                   by = "1 day")) %>% 
-            mutate(date_id = map(date, 
-                                 function(x){
-                                       x %>% as.character() %>% 
-                                             str_replace_all("-", "")
-                                             
-                                 }))
-            
+            mutate(date_id = map_int(date, 
+                                     function(x){
+                                           x %>% as.character() %>% 
+                                                 str_replace_all("-", "") %>% 
+                                                 as.integer()
+                                    }
+                              ))
       
       
       # pull historical data: 
       census <- extract_census(historical_start_id, 
                                historical_end_id, 
-                               n_units = n_unit_param)
+                               n_units = n_unit) %>% 
+            ungroup() %>%  # convert from grouped_df to df
+            right_join(full_date_list)
+      
+      # todo: join on full date list: 
       
       
-      
-      
+      return(census)
       
 }
 
@@ -74,6 +80,8 @@ census_forecast <- function(startdate_id,
 
 
 # test the function: ------
-census_forecast("20181201", 
-                "20190501", 
-                "LGH 6E")
+census_fcast <- census_forecast("20181201", 
+                                "20190501", 
+                                n_unit = "LGH 6E")
+
+str(census_fcast)
